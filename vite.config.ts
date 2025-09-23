7d8bc23f623a6f9fdb2d1dfd1a6c199deb6b4d01
Krzysztof-Jaminski <krzysztofjofficial@gmail.com>
2025-09-23 12:49:52 +0200

    Fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
@@ -11,6 +11,10 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
     proxy: {
       '/api': {
         target: process.env.VITE_API_BASE_URL || 'https://drzewaapi-app-2024.azurewebsites.net',
